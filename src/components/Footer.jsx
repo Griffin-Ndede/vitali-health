74fbcc7da3b0f5bb36aedaaecd4e021867f9f414
Griffin <omondigriffin4@gmail.com>
2024-12-04 05:02:48 +0300

    changes to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -38,6 +38,11 @@ function Footer() {
         },
         () => {
           failureAlert();
+          setFormData({
+            email: '',
+            name: '',
+            phone: '',
+          })
         },
       );
         // function to show success alert prompt
